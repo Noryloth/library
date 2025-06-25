7fbd17520ed16d52c230fb4cd6ac95639cb8c8a3
Katsuya <noryloth@gmail.com>
2025-06-25 13:47:56 +0800

    Add new book button and show dialog form function

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // DOM
 const books = document.querySelector('.books');
+const newBookBtn = document.getElementById('new-book');
+const dialog = document.getElementById("dialog");
 
 
 // Books array
-const myLibrary = ["The Hobbit", "Captain Blood: His Odyssey", "The Black Corsair", "The King in Yellow"];
+const myLibrary = [];
 
 
 // Book constructor
@@ -33,6 +35,16 @@ function displayBooks(myLibrary) {
 displayBooks(myLibrary);
 
 
+// Add new book form
+newBookBtn.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+// Add new book close button
+
+
+
+// Add books to a library array
 function addBookToLibrary(title, author, pages, read) {
     // take parameters, create a book and then store it in the array
 
